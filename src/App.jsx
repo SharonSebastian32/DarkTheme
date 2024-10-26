656ea8348f98163a3a014400591c62880dba7e16
SharonSebastian32 <sharonsebastian81@gmail.com>
2024-10-26 16:06:01 +0530

    add or remove light theme function are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
- 
-function App() {
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* {themeButton} */}
-        </div>
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./Contexts/Theme";
 
-        <div className="w-full max-w-sm mx-auto"></div>
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add("themeMode");
+  }, []);
+
+  return (
+    <ThemeProvider vale={{ themeMode, darkTheme, lightTheme }}>
+      {" "}
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* {themeButton} */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
