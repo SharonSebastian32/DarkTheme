43de8931a52e1d7b3bfc7074cb713868645b8c15
SharonSebastian32 <sharonsebastian81@gmail.com>
2024-10-26 16:59:41 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { ThemeProvider } from "./Contexts/Theme";
+import Card from "./Components/Card";
+import ThemeBtn from "./Components/ThemeButton";
 
 function App() {
   const [themeMode, setThemeMode] = useState("light");
@@ -15,20 +17,19 @@ function App() {
 
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add("themeMode");
-  }, []);
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider vale={{ themeMode, darkTheme, lightTheme }}>
-      {" "}
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* {themeButton} */}
+            <ThemeBtn />
           </div>
-          <h1>Hey Sharon</h1>
-
-          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
